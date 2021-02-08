f6431706b5e85271ea78dff8b0c3a624a43d2c9b
Aapdewolff <jasondewolff@gmail.com>
2021-02-08 19:49:31 +0100

    Add vector2, vector3 a basic mathf class and type checks

--- Main.js
+++ Main.js
@@ -1,7 +1,14 @@
 window.onload = function() {
     LibBuilder.Build(function() {
-        Canvas.Init();
+        Canvas.init();
 
-        window.onresize = Canvas.Resize;
+        window.onresize = Canvas.resize;
+
+        var vec3A = new Vector3(3, 10, 30);
+        vec3A.onChange = function() { console.log("Change!"); }
+        console.log(vec3A.magnitude);
+        vec3A.magnitude = 3;
+        console.log(vec3A.magnitude);
+        console.log(vec3A.toString());
     });
 }
